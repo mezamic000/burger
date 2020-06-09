33a950fe5bfd5cffdc2e14be1bce971bd50ab2c5
Firecraker <mezamic000@gmail.com>
2020-06-09 11:47:57 -0500

    added connection to express, routes,  handlebars

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+var PORT = process.env.PORT || 8080;
+
 app.use(express.static("public"));
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true })); //change bodyParser to express?
+app.use(bodyParser.json());
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller");
+app.use(routes);
+
+app.listen(PORT, function () {
+	console.log("App listening on PORT" + PORT);
+});
