db4568a240c3cb2797c274ca713b6c0e144f4593
Firecraker <mezamic000@gmail.com>
2020-08-07 10:03:34 -0500

    removed bodyparser with express

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
-
-app.use(bodyParser.urlencoded({ extended: true })); //change bodyParser to express?
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller");
+
 app.use(routes);
 
 app.listen(PORT, function () {
-	console.log("App listening on PORT" + PORT);
+  console.log("App listening on PORT" + PORT);
 });
